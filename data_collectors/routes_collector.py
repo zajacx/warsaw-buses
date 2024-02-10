e2a85b90f888649777ec8ab58903448f7b143547
Tomasz Zając <tz448580@students.mimuw.edu.pl>
2024-02-10 18:55:20 +0100

    Small static data collected

---
+++ data_collectors/routes_collector.py
@@ -0,0 +1,16 @@
+#from tools import force_response
+import requests
+import json
+import os
+
+api_key = "d5a0df57-dbff-4d5b-b580-5949e1750cde"
+url = 'https://api.um.warszawa.pl/api/action/public_transport_routes/'
+
+request_params = {
+    'apikey': api_key
+}
+
+response = requests.get(url, params=request_params).json()
+
+with open(os.path.join('../static_data', 'routes.json'), 'w') as file:
+    json.dump(response["result"], file, indent=2)
